029b1c329671ba632c5c9d5642d6a95075ade8dd
X3ZvaWQ <x3zvawq@gmail.com>
2021-04-29 23:53:26 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import core, { effect$, closeForeground, setSystemUiVisibility } from '@auto.pro/core'
+
+core({
+    baseWidth: 1920,
+    baseHeight: 1080,
+    needCap: true,
+    needService: true,
+    needForeground: true
+});
+
+
